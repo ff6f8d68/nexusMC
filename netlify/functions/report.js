9e6a4e60cbc0eeebebe304bd030f3e83f3a0f0da
unknow <138153020+ff6f8d68@users.noreply.github.com>
2024-09-14 10:54:33 -0700

    Update report.js

--- netlify/functions/report.js
+++ netlify/functions/report.js
@@ -1,48 +1,40 @@
 exports.handler = async function(event, context) {
-  // Dynamically import the node-fetch module
-  const fetch = (await import('node-fetch')).default;
+  if (event.httpMethod === 'POST') {
+    const data = JSON.parse(event.body);
+    const fetch = (await import('node-fetch')).default;
+    // Your Discord webhook URL
+    const discordWebhookUrl = 'https://discord.com/api/webhooks/1284559062859518014/kac428QnDZZlEnJxL-WSEvx1WOrNKjLPg4cNhKAL4xmkIjI4DkqJ0BlI-wi0YsXcn8ah';
 
-  // Check if there's a content query parameter
-  const url = new URL(event.rawUrl, `https://${event.headers.host}`);
-  const content = url.searchParams.get('content');
-
-  if (!content) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ error: 'No content parameter provided' })
+    // Prepare the payload for Discord
+    const payload = {
+      content: `Type: ${data.Type}\nEmail: ${data.email}\nMessage: ${data.Report}`
     };
-  }
 
-  // Your Discord webhook URL
-  const discordWebhookUrl = 'https://discord.com/api/webhooks/1284559062859518014/kac428QnDZZlEnJxL-WSEvx1WOrNKjLPg4cNhKAL4xmkIjI4DkqJ0BlI-wi0YsXcn8ah';
-
-  // Prepare the payload for Discord
-  const payload = {
-    content: content
-  };
-
-  // Send the message to Discord
-  try {
-    await fetch(discordWebhookUrl, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(payload)
-    });
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Failed to send message to Discord' })
-    };
-  }
-
-  // Construct the mailto link
-  const mailtoLink = `mailto:nexusmcreports@proton.me?subject=Support%20Query&body=${encodeURIComponent(content)}`;
-
-  // Redirect to the mailto link
-  return {
-    statusCode: 302,
-    headers: {
-      Location: mailtoLink
+    // Send the message to Discord
+    try {
+      await fetch(discordWebhookUrl, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(payload)
+      });
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'Failed to send report' })
+      };
     }
-  };
+
+    // Redirect to the mailto link
+    return {
+      statusCode: 302,
+      headers: {
+        Location: `mailto:nexusmcreports@proton.me?subject=Support%20Query&body=${encodeURIComponent(data.Report)}`
+      }
+    };
+  } else {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({ error: 'Method not allowed' })
+    };
+  }
 };
